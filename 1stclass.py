8e6cd148bfc5b695771cf2ca4cd2b30d3560b3eb
Shreyadeep-code <shreyadeepparida@gmail.com>
2026-02-17 18:18:38 +0530

    1stclass.py

---
+++ 1stclass.py
@@ -0,0 +1,58 @@
+name = "Shreyadeep Parida"
+age = 20
+print(name)
+print(name.count("p"))
+print(age)
+print("my name is ",name)
+print("my age is :",age)
+print(78)
+print(type(name))
+print(type(age))
+print("4 data types integer,string,float,boolean,none")
+a = None
+answer = True
+print(type(answer))
+print(type(a))
+s =4
+k =2
+"""sum =k+s
+print("sum is :",sum)
+diff =k-s
+print("difference is :",diff)
+product =k*s"""
+#print("Product is:",product)
+#arithmetic operator
+print(s+k)
+print(s*k)
+print(k-s)
+print(s/k)
+print(s%k) #remainder
+print(s**k) # power
+# relational/ comparision operator >=,<,>
+print(s==k)
+print(s!=k)
+print(s>=k)
+#assignment operator
+s +=10
+print(s)
+#logical operator and,or,not
+t = True
+f = False
+print("and operator :",t and f)
+print("or operator :",t or f)
+print(not (s==k))
+print((s!=k) and (s > k))
+#type conversion
+h =int("6")
+g =8.9
+print(type(h))
+add = h+g
+print(add)
+"""g = str(g)
+print(type(g))"""
+name = input("enter your name :")
+age= int(input("enter your age :"))
+marks = float(input("enter your marks :"))
+print("welcome ",name)
+print(" age=",age)
+print("marks=",marks)
---
+++ 2ndclass.py
@@ -0,0 +1,33 @@
+s1 ="shreyadeep is good boy.\nuse python"
+print(s1)
+s2 ='ram is \t'
+s3 =" good boy"
+print(s2.upper())
+print(s2+s3)
+print(len(s3))
+print(s1[6])
+print(s1[0:10])
+print(s2[-4:-1])
+print(s2.endswith("pl"))
+print(s2.capitalize())
+print(s2)
+s2 =s2.capitalize()
+print(s2)
+print(s3.replace("good","bad"))
+print(s2.find("is"))
+print(s3.count("o"))
+N =input("enter your name :")
+print(len(N))
+w = "$ is avery good $ used in america called $."
+print("$ used times ",w.count("$"))
+age =int(input("enter your age :"))
+if(age>18):
+    print("can vote")
+elif(age==18):
+    print("you can apply for voter id")
+else:
+    print("not eligible for vote")
+
+
+
+
