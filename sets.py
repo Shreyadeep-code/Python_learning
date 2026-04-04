3a88d61ff400134f9ad7089e7355ed7cf01a5733
Shreyadeep-code <shreyadeepparida@gmail.com>
2026-04-04 20:06:16 +0530

    sets_study

---
+++ sets.py
@@ -0,0 +1,19 @@
+#set elemets are not changeable.
+#repeated elements in the set stored once.
+set ={1,2,3,3,4,5}
+print(set)
+
+#print(set.clear())#clear the set
+set.add(8)
+set.remove(3)
+set.add("ram")
+set.add((6,7,9))
+#lists can't be added in set. 
+set.pop() #removes a random element
+
+print(set)
+print(len(set))
+"""set1 ={'ty',56.82,2} 
+print(set.union(set1))
+print(set.intersection(set1))"""
+
